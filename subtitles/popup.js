6629e8034663e29b75dde2b3cebfe6cfb052aa97
Israel Karpel <59684104+IsraelKarpel@users.noreply.github.com>
2021-09-12 01:11:14 +0300

    Update popup.js

--- subtitles/popup.js
+++ subtitles/popup.js
@@ -1,11 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').addEventListener('click', onclick, false)
-    
-    function onclick () {
-        chrome.tabs.query({currentWindow: true, active: true},
-        function (tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, 'hi')  
-        })
-    }
-}, false)
-    
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('button').addEventListener('click', () => {
+    chrome.tabs.query({currentWindow: true, active: true},
+      (tabs) => chrome.tabs.sendMessage(tabs[0].id, 'hi'))
+  });
+});
